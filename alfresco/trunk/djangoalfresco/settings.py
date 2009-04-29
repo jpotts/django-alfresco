380ba537b5f542229b9f45e24205c417bb7bc592
screeley <screeley@f77b4796-5b8c-11de-b36e-3fc19fc75de0>
2009-04-29 15:02:53 +0000

    settings change.

--- alfresco/trunk/djangoalfresco/settings.py
+++ alfresco/trunk/djangoalfresco/settings.py
@@ -76,7 +76,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    DIRNAME + "/html/templates/",
+    DIRNAME + "/html/templates/sample/",
+    DIRNAME + "/html/templates/base/",
 )
 
 INSTALLED_APPS = (
