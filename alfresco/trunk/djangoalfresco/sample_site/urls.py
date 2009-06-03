7b68113c984f3c13eb9c5f7fb32607221e968174
jpotts <jpotts@f77b4796-5b8c-11de-b36e-3fc19fc75de0>
2009-06-03 21:09:09 +0000

    Latest-and-greatest fixtures and corresponding ACP file.

--- alfresco/trunk/djangoalfresco/sample_site/urls.py
+++ alfresco/trunk/djangoalfresco/sample_site/urls.py
@@ -1,15 +1,24 @@
 from django.conf.urls.defaults import *
 from hierarchies.feeds import CategoryFeed
+from hierarchies.models import Category, Hierarchy
 
 feeds = {
     'categories': CategoryFeed,
 }
 
 urlpatterns = patterns('',
-    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+    url(r'^site_map/$', 'django.views.generic.simple.direct_to_template', {
+        'template' : 'site_map.html' }, name='site_map'),
+    url(r'^$', 'sample_site.views.home', name='home'),
+    url(r'^static/(?P<id>[-\w]+)/$', 'sample_site.views.static_content', name='static_detail'),
     url(r'^content/(?P<id>[-\w]+)/$',   'sample_site.views.content', name='content_detail'),
     url(r'^content/print_view/(?P<id>[-\w]+)/$',   'sample_site.views.print_view', name='print_view'),
-    url(r'^static/(?P<id>[-\w]+)/$', 'sample_site.views.static_content', name='static_detail'),
+    url(r'^(?P<slug>[-\w]+)/$','sample_site.views.hierarchy_detail', 
+        {'queryset' : Hierarchy.objects.all(), 'slug_field' : 'slug'}, name='hierarchy_detail'),
+    url(r'^(?P<path>.*)/content/(?P<id>[-\w]+)/$$', 'sample_site.views.category_content_detail', name='category_content_detail'),
+    url(r'^(?P<path>.*)/$', 'sample_site.views.category_detail', 
+        {'queryset' : Category.objects.all(), 'slug_field' : 'slug'}, name='category_detail'),
+    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
     url(r'^image/(?P<path>.*)',   'sample_site.views.photo', name='image'),
     url(r'^search/$',   'sample_site.views.search', name='search'),
     url(r'^tag_search/$',   'sample_site.views.tag_search', name='tag_search'),
--- alfresco/trunk/djangoalfresco/urls.py
+++ alfresco/trunk/djangoalfresco/urls.py
@@ -14,14 +14,7 @@ urlpatterns = patterns('',
     url(r'^admin/alfresco/cache/', 'alfresco.views.cache', name='alfresco_cache'),
     url(r'^admin/(.*)', admin.site.root, name='admin_home'),
     (r'^alfresco/', include('alfresco.urls')),
-    (r'^sample_site/', include('sample_site.urls')),
-    url(r'^site_map/$', 'django.views.generic.simple.direct_to_template', {
-        'template' : 'site_map.html' }, name='site_map'),
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    url(r'^$', 'sample_site.views.home', name='home'),
-    url(r'^(?P<slug>[-\w]+)/$','sample_site.views.hierarchy_detail', 
-        {'queryset' : Hierarchy.objects.all(), 'slug_field' : 'slug'}, name='hierarchy_detail'),
-    url(r'^(?P<path>.*)/content/(?P<id>[-\w]+)/$$', 'sample_site.views.category_content_detail', name='category_content_detail'),
-    url(r'^(?P<path>.*)/$', 'sample_site.views.category_detail', 
-        {'queryset' : Category.objects.all(), 'slug_field' : 'slug'}, name='category_detail'),
+    (r'^sample_site/', include('sample_site.urls')),
+    url(r'^$', 'sample_site.views.home', name='home')
 )
