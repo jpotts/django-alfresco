85431cedfe62a75bbd7a48e0a817fabc5ac7c46f
screeley <screeley@f77b4796-5b8c-11de-b36e-3fc19fc75de0>
2009-06-08 22:45:24 +0000

    We don't use it.

--- alfresco/trunk/djangoalfresco/hierarchies/urls/main.py
+++
@@ -1,8 +0,0 @@
-#
-# Copyright 2009 Optaros, Inc.
-#
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'index/(?P<path>.*)/', 'sample_site.views.category_index', {}, "category-index"),
-)
